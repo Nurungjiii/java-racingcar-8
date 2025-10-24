815931bda7b0314727a7b514d1407ced688fce5d
Nurungjiii <choawon0330@gmail.com>
2025-10-24 17:33:03 +0900

    feat(Car) : 자동차 이름 유효성 검사 메서드 추가 (validateName)
    - validateName 메서드를 추가하여 null/이름 5자 초과 인경우 검사
    - IllegalArgumentException을 발생. 유효한 Car 객체만 생성되도록 보장

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,16 +1,32 @@
 package racingcar;
 
 public class Car {
+    // 이름 길이 제한을 상수 정의
+    private static final int MAX_NAME_LENGTH = 5;
     // 이름,위치,속성 정의
     // 이름은 한 번 정해지면 바뀌지 않음으로 final로 선언
     private final String name;
     private int position;
 
     public Car(String name) {
+        // 생성자에서 이름 할당 전에 유효성 검사 수행
+        validateName(name);
         // 전달 받은 이름으로 필드 초기화
         this.name=name;
         // 모든 자동차의 시작 위치는 0
         this.position=0;
+    }
+
+    // 이름 유효성 검사 메서드 구현
+    private void validateName(String name) {
+        // 이름이 null이거나, 앞뒤 공백을 제거했을 때 비어있는지 검사
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 공백");
+        }
+        // 이름의 길이가 5자를 초과하는지 검사
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 길이5초과");
+        }
     }
 
     public String getName() {
